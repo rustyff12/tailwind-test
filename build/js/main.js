6b7e0837fa0c83fcea8c5aadae88e26379a5a0ac
rustyff12 <rustyff123@gmail.com>
2024-02-25 18:01:57 +0100

    js added. custom hamburger menu animations added

---
+++ build/js/main.js
@@ -0,0 +1,29 @@
+/* const initApp = () => {
+    const hambergerBtn = document.getElementById("hamburger-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle("hidden");
+        mobileMenu.classList.toggle("flex");
+    };
+
+    hambergerBtn.addEventListener("click", toggleMenu);
+    mobileMenu.addEventListener("click", toggleMenu);
+};
+document.addEventListener("DOMContentLoaded", initApp); */
+
+const initApp = () => {
+    const hamburgerBtn = document.getElementById("hamburger-button");
+    const mobileMenu = document.getElementById("mobile-menu");
+
+    const toggleMenu = () => {
+        mobileMenu.classList.toggle("hidden");
+        mobileMenu.classList.toggle("flex");
+        hamburgerBtn.classList.toggle("toggle-btn");
+    };
+
+    hamburgerBtn.addEventListener("click", toggleMenu);
+    mobileMenu.addEventListener("click", toggleMenu);
+};
+
+document.addEventListener("DOMContentLoaded", initApp);
